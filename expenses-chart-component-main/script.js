6e1c85795e7c8a285c2567a19f22d86fffa2acc0
alex931d <102577495+alex931d@users.noreply.github.com>
2022-12-13 11:47:12 +0100

    t

--- expenses-chart-component-main/script.js
+++ expenses-chart-component-main/script.js
@@ -10,9 +10,10 @@ circle.forEach(tab => {
             element.classList.remove('active');
             
         });
-       point = e.target;
-       if (e.target.classList.contains('.circle')) {
-          e.target.classList.add('active');
+     
+       if (e.target.classList.contains('circle')) {
+          e.target.classList.add('active'); 
+           point = e.target;
        }
       
         
